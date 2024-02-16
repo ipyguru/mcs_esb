055db582f4c9d5bea7e418418c26ea3b1e5597f5
ipyguru <vbuots@icloud.com>
2024-02-16 12:24:09 +0400

    create customer products migration

--- core/models/customer_product.py
+++ core/models/customer_product.py
@@ -1,12 +1,21 @@
 from .base import Base
 from sqlalchemy.orm import Mapped, mapped_column
-from sqlalchemy import String
+from sqlalchemy import String, Text, ForeignKey
 
 
 class CustomerProduct(Base):
     __tablename__ = "customer_products"
 
-    name: Mapped[str] = mapped_column(String(150))
+    name: Mapped[str] = mapped_column(
+        Text,
+        default="",
+        server_default="",
+    )
+
+    product_id: Mapped[int] = mapped_column(
+        ForeignKey("products.id"),
+        nullable=False,
+    )
 
     def __repr__(self):
         return f"<CustomerProduct(name={self.name})>"
