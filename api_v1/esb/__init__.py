d3247457d20d06def64e27f12e0a659d546c9487
ipyguru <vbuots@icloud.com>
2024-03-05 22:16:56 +0400

    add reconnect if connection is broke

--- api_v1/esb/__init__.py
+++ api_v1/esb/__init__.py
@@ -2,6 +2,7 @@ import json
 from typing import List, Any
 
 import pika
+from pika.exceptions import ConnectionClosed
 
 from api_v1.esb.schemas import Package, GetMessages, PackageMessage, Ask
 from core.settings import settings
@@ -10,13 +11,25 @@ from core.settings import settings
 class RabbitMQManager:
     def __init__(self):
         self.host = settings.rabbit.host
-        self.connection = pika.BlockingConnection(pika.ConnectionParameters(self.host))
-        self.channel = self.connection.channel()
+        self.params = pika.ConnectionParameters(self.host)
+        self.connection = None
+        self.channel = None
+
+    def connect(self):
+        if not self.connection or self.connection.is_closed:
+            self.connection = pika.BlockingConnection(self.params)
+            self.channel = self.connection.channel()
 
     def __del__(self):
-        self.connection.close()
+        if self.connection and self.connection.is_open:
+            self.connection.close()
 
     def publish_message(self, package: Package, message: List[Any]):
+        if not self.connection or self.connection.is_closed:
+            self.connect()
+            # Нужно подождать, пока канал будет открыт
+            while not self.connection.is_open:
+                continue
         try:
             self.channel.basic_publish(
                 exchange=package.exchange,
@@ -31,27 +44,43 @@ class RabbitMQManager:
             raise Exception(error)
 
     def get_messages(self, query: GetMessages):
+        if not self.connection or self.connection.is_closed:
+            self.connect()
+            # Нужно подождать, пока канал будет открыт
+            while not self.connection.is_open:
+                continue
+
         package_message = PackageMessage(messages=[])
-        cnt = self.channel.queue_declare(
-            queue=query.queue, passive=True
-        ).method.message_count
-        if cnt:
-            for count_massages in range(cnt):
-                method_frame, header_frame, body = self.channel.basic_get(
-                    query.queue, auto_ack=False
-                )
-                if method_frame:
-                    body = {
-                        "message": json.loads(body),
-                        "delivery_tag": method_frame.delivery_tag,
-                    }
-                    package_message.messages.append(body)
-                    # self.channel.basic_ack(method_frame.delivery_tag)
-                else:
-                    break
+        try:
+            cnt = self.channel.queue_declare(
+                queue=query.queue, passive=True
+            ).method.message_count
+            if cnt:
+                for count_massages in range(cnt):
+                    method_frame, header_frame, body = self.channel.basic_get(
+                        query.queue, auto_ack=False
+                    )
+                    if method_frame:
+                        body = {
+                            "message": json.loads(body),
+                            "delivery_tag": method_frame.delivery_tag,
+                        }
+                        package_message.messages.append(body)
+                    else:
+                        break
+        except Exception as e:
+            error = f"Ошибка получения сообщения из очереди:\n {e}"
+            raise Exception(error)
+
         return package_message
 
     def ask_messages(self, query: Ask):
+        if not self.connection or self.connection.is_closed:
+            self.connect()
+            # Нужно подождать, пока канал будет открыт
+            while not self.connection.is_open:
+                continue
+
         cnt = len(query.delivery_tags)
         for tag in query.delivery_tags:
             try:
